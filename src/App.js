cebc66f4f7ae1051012ad2d63de3d9f04d6ef49f
Aniedi Ubokudom <aniedi.anthonyu@yahoo.com>
2023-08-31 19:43:32 -0400

    non functional employee directory

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './app.css';
+import HomePage from './components/HomePage';
+import EmployeePage from './components/EmployeePage';
+
+export default function App() {
+
+  return (
+    <div className="app">
+      <h1>App</h1>
+      <HomePage />
+      <EmployeePage />
+    </div>
+  );
+}
---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import './searchBar.css'
+
+export default function SearchBar() {
+  return (
+    <div className='searchbar'>
+       <input placeholder='Search' />
+    </div>
+  )
+}
